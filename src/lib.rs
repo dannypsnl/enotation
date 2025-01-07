6477069ad2977f6fcd668d9ab1064a3aac5f4c7c
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-07 15:33:16 +0800

    quoting
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::rc::Rc;
+
 use pest::{iterators::Pair, Parser};
 use pest_derive::Parser;
 
@@ -9,6 +11,18 @@ pub enum ENotation {
     Identifier(String),
     // (a b c)
     List(Vec<ENotation>),
+    // '(1 c a)
+    Quote(Rc<ENotation>),
+    // `(1 c a)
+    QuasiQuote(Rc<ENotation>),
+    Unquote(Rc<ENotation>),
+    UnquoteSplicing(Rc<ENotation>),
+    // #'(1 c a)
+    Syntax(Rc<ENotation>),
+    // #`(1 c a)
+    QuasiSyntax(Rc<ENotation>),
+    Unsyntax(Rc<ENotation>),
+    UnsyntaxSplicing(Rc<ENotation>),
 }
 
 #[derive(Parser)]
@@ -22,16 +36,36 @@ fn parse_value(pair: Pair<Rule>) -> ENotation {
         Rule::list => ENotation::List(pair.into_inner().map(parse_value).collect()),
         Rule::int => ENotation::Integer(pair.as_str().parse().unwrap()),
         Rule::identifier => ENotation::Identifier(pair.as_str().to_string()),
+        Rule::quote => ENotation::Quote(Rc::new(parse_value(pair.into_inner().peek().unwrap()))),
+        Rule::quasiquote => {
+            ENotation::QuasiQuote(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
+        Rule::unquote => {
+            ENotation::Unquote(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
+        Rule::unquote_splicing => {
+            ENotation::UnquoteSplicing(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
+        Rule::syntax => ENotation::Syntax(Rc::new(parse_value(pair.into_inner().peek().unwrap()))),
+        Rule::quasisyntax => {
+            ENotation::QuasiSyntax(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
+        Rule::unsyntax => {
+            ENotation::Unsyntax(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
+        Rule::unsyntax_splicing => {
+            ENotation::UnsyntaxSplicing(Rc::new(parse_value(pair.into_inner().peek().unwrap())))
+        }
         Rule::COMMENT
         | Rule::WHITESPACE
         | Rule::dec_int
         | Rule::single_line_comment
-        | Rule::boolean => {
+        | Rule::boolean
+        | Rule::paren_list
+        | Rule::bracket_list
+        | Rule::notation => {
             unreachable!()
         }
-        Rule::paren_list => todo!(),
-        Rule::bracket_list => todo!(),
-        Rule::notation => todo!(),
     }
 }
 
@@ -74,4 +108,17 @@ fn parse_identifier() {
     use ENotation::*;
     let output = parse_str("abc");
     assert_eq!(output, Identifier("abc".to_string()));
+}
+
+#[test]
+fn parse_quoting() {
+    use ENotation::{Integer as I, List as L, QuasiQuote as QQ, Quote as Q, Syntax as S};
+    let output = parse_str("'(1 2 3)");
+    assert_eq!(output, Q(L(vec![I(1), I(2), I(3)]).into()));
+
+    let output = parse_str("`(1 2 3)");
+    assert_eq!(output, QQ(L(vec![I(1), I(2), I(3)]).into()));
+
+    let output = parse_str("#'(1 2 3)");
+    assert_eq!(output, S(L(vec![I(1), I(2), I(3)]).into()));
 }
