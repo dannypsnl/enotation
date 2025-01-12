0d204580c27dbe2363f8efb06b16bccbadf96b8a
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-01-12 11:16:05 +0800

    load back span (location) information
    Signed-off-by: Lîm Tsú-thuàn <inbox@dannypsnl.me>

--- src/lib.rs
+++ src/lib.rs
@@ -20,21 +20,76 @@ mod tests;
 pub struct ENotationParser;
 
 #[derive(Debug, FromPest)]
-#[pest_ast(rule(Rule::notation))]
-pub enum ENotation {
+#[pest_ast(rule(Rule::notation_))]
+pub enum ENotationBody {
     Literal(Literal),
     Container(Container),
     Quoting(Quoting),
     Syntaxing(Syntaxing),
 }
 
+#[derive(Debug, FromPest)]
+#[pest_ast(rule(Rule::notation))]
+pub struct ENotation {
+    #[pest_ast(outer(with(DiagnosticSpan::from_pest_span)))]
+    pub span: DiagnosticSpan,
+    pub body: ENotationBody,
+}
+
+#[derive(Debug, Clone, PartialEq)]
+pub struct DiagnosticSpan {
+    pub start_line: usize,
+    pub start_col: usize,
+    pub start_offset: usize,
+
+    pub end_line: usize,
+    pub end_col: usize,
+    pub end_offset: usize,
+
+    pub span: String,
+}
+
+impl Default for DiagnosticSpan {
+    fn default() -> Self {
+        Self {
+            start_line: 0,
+            start_col: 0,
+            start_offset: 0,
+            end_line: 0,
+            end_col: 0,
+            end_offset: 0,
+            span: String::new(),
+        }
+    }
+}
+
+impl DiagnosticSpan {
+    pub fn from_pest_span(span: pest::Span<'_>) -> Self {
+        let (start_line, start_col) = span.start_pos().line_col();
+        let (end_line, end_col) = span.end_pos().line_col();
+        let start_offset = span.start();
+        let end_offset = span.end();
+        let source = span.as_str().to_string();
+        Self {
+            start_line,
+            start_col,
+            start_offset,
+            end_line,
+            end_col,
+            end_offset,
+            span: source,
+        }
+    }
+}
+
 impl Display for ENotation {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            ENotation::Literal(l) => write!(f, "{}", l),
-            ENotation::Container(c) => write!(f, "{}", c),
-            ENotation::Quoting(q) => write!(f, "{}", q),
-            ENotation::Syntaxing(s) => write!(f, "{}", s),
+        use ENotationBody::*;
+        match &self.body {
+            Literal(l) => write!(f, "{}", l),
+            Container(c) => write!(f, "{}", c),
+            Quoting(q) => write!(f, "{}", q),
+            Syntaxing(s) => write!(f, "{}", s),
         }
     }
 }
